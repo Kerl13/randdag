8626e301759fbbf11a9f11732f687f9a1f6469e5
Martin Pépin <kerl@wkerl.me>
2020-11-29 11:32:11 +0100

    Ignore a warning

--- lib/argtable-3.1.5/argtable3.mk
+++ lib/argtable-3.1.5/argtable3.mk
@@ -1,4 +1,11 @@
 $(BUILD)argtable.o: lib/argtable-3.1.5/argtable3.h
 $(BUILD)argtable.o: lib/argtable-3.1.5/argtable3.c
 	@mkdir -p "$(BUILD)"
-	$(CC) $(CFLAGS) -o $@ -c lib/argtable-3.1.5/argtable3.c
+	$(CC) $(CFLAGS) -Wno-clobbered -o $@ -c lib/argtable-3.1.5/argtable3.c
+
+
+# Note on the -Wno-clobbered flag
+#
+# A warning is issued by GCC in by the trex_compile function, see :
+# https://github.com/argtable/argtable3/issues/53.
+# We can safely ignore it as long as we do not use the arg_rex* stuff.
