f6bfb88f3279d96cdd38c36e5d63bb1bd081efcd
Martin Pépin <kerl@wkerl.me>
2025-04-14 10:37:46 +0200

    Update the examples

--- examples/doag_count.c
+++ examples/doag_count.c
@@ -31,70 +31,27 @@ const char *usage_string = "USAGE: %s N M K\nComputes the number of DOAGs with "
 
 int main(int argc, char **argv) {
   int n, m, k;
+  memo_t memo;
+  mpz_t *res;
 
   /* Get the size parameters from the command line */
   if (parse_three_ints(usage_string, argc, argv, &n, &m, &k) != 0)
     return 1;
 
-  /* We check here the condition under which a DOAG with n vertices, m edges,
-   * and k sources exists. */
-  if (n != 1 && k >= n) {
-    printf("For n > 1, there must be less sources than vertices.\n");
-  } else if (n - 1 > m) {
-    printf("If m < n - 1, there are not enough edges for the graph to be "
-           "connected.\n");
-  } else {
-    const int upper_bound = k * (n - k) + (n - k) * (n - k - 1) / 2;
-    if (m > upper_bound) {
-      printf(
-          "The number of edges of a DOAG with n sources and k sources cannot "
-          "exceed k * (n - k) + choose(n - k, 2) (= %d here).\n",
-          upper_bound);
-    } else {
-      /* Here we are sure that there exists a DOAG with the given parameters.
-       * Note that it is important to ensure that this is the case, otherwise
-       * the behaviour of doag_count is undefined. */
-      int M, N;
-      mpz_t *res, tmp;
+  /* Allocate the memoisation table. */
+  memo = memo_alloc(n, m, -1);
 
-      /* Allocate a big enough memoisation table.
-       * FIXME: explain the purpose of this third parameter. */
-      memo_t memo = memo_alloc(n, m, 0);
+  /* Count! */
+  res = doag_count(memo, n, m, k, -1);
 
-      /* Count! */
-      res = doag_count(memo, n, m, k, 0);
+  /* Print the result to stdout */
+  printf("The number of DOAGs with %d vertices, %d edges, and %d sources is: ",
+         n, m, k);
+  mpz_out_str(stdout, 10, *res);
+  printf("\n");
 
-      /* Print the result to stdout */
-      printf(
-          "The number of DOAGs with %d vertices, %d edges, and %d sources is: ",
-          n, m, k);
-      mpz_out_str(stdout, 10, *res);
-      printf("\n");
-
-      /* Print some other interesting values */
-      M = m;
-      N = n;
-      mpz_init(tmp);
-      printf("Here is the total number of DOAGs with m vertices, 1 source "
-             "only, and any number of vertices, for some "
-             "values of m:\n");
-      for (m = 1; m <= M && m <= N - 1 && m <= 50; m++) {
-        mpz_set_ui(tmp, 0);
-        for (n = 2; n <= m + 1; n++) {
-          if (m <= n * (n - 1) / 2)
-            mpz_add(tmp, tmp, *doag_count(memo, n, m, 1, 0));
-        }
-        printf("%d: ", m);
-        mpz_out_str(stdout, 10, tmp);
-        fflush(stdout);
-        printf("\n");
-      }
-      mpz_clear(tmp);
-
-      /* Free up the space occupied by the memoisation table */
-      memo_free(memo);
-    }
-  }
+  /* Free up the space occupied by the memoisation table */
+  memo_free(memo);
 
   return 0;
 }
--- examples/doag_n.c
+++ examples/doag_n.c
@@ -26,30 +26,28 @@ const char *usage_string = "USAGE: %s N\nSamples a DOAG of size N.\n";
 int main(int argc, char **argv) {
   int n;
   gmp_randstate_t prng;
+  randdag_t doag;
 
   /* Get the number of vertices from the command line */
   if (parse_one_int(usage_string, argc, argv, &n) != 0)
     return 1;
 
-  /* Don't forget to initialise the PRNG */
+  /* Initialise the PRNG */
   gmp_randinit_default(prng);
   gmp_randseed_ui(prng, 0xdeadbeef);
 
-  /* The interesting part is here. */
-  {
-    /* Generate a uniform DOAG with n vertices. */
-    randdag_t doag = doag_unif_n(prng, n);
+  /* Generate a uniform DOAG with n vertices. */
+  doag = doag_unif_n(prng, n);
 
-    /* Print the graph in dot format to stdout.
-     * Since the ordering of the nodes matters in DOAGs, the RD_DOT_ORDERING
-     * flag must be passed to tell graphviz to respect our ordering when
-     * printing the graph. */
-    randdag_to_dot(stdout, doag, RD_DOT_ORDERING);
+  /* Print the graph in dot format to stdout.
+   * Since the ordering of the nodes matters in DOAGs, the RD_DOT_ORDERING
+   * flag must be passed to tell graphviz to respect our ordering when
+   * printing the graph. */
+  randdag_to_dot(stdout, doag, RD_DOT_ORDERING);
 
-    /* Free up all the space taken by the doag and GMP's prng. */
-    randdag_free(doag);
-    gmp_randclear(prng);
-  }
+  /* Free up all the space taken by the doag and GMP's prng. */
+  randdag_free(doag);
+  gmp_randclear(prng);
 
   return 0;
 }
